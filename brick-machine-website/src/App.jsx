cba27749ceac79b98fae758e20bb1bb5adf1d61c
Asif Khan <asifkl786@gmail.com>
2026-03-05 17:58:15 +0530

    second commit

--- brick-machine-website/src/App.jsx
+++ brick-machine-website/src/App.jsx
@@ -2,12 +2,16 @@
 import React from 'react';
 import Landing from './components/Landing';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
+    //   <Landing />
+    // </div>
+     <BrowserRouter>
       <Landing />
-    </div>
+    </BrowserRouter>
   );
 }
 
